9591e9763ca531790fb22a1c76ca41b7944a3e43
Harsh Tyagi <tyagih7654321@gmail.com>
2025-06-22 23:07:09 +0530

    Final Code

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Suspense } from "react";
 import CalculatorContent from "@/components/CalculatorContent";
 import AdPlaceholder from "@/components/AdPlaceholder";
